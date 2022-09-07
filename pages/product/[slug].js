315d0c4d4e1316b6df520205a076393884dab7aa
SadiQ HabiL <kingqabil@live.com>
2022-09-07 18:30:30 +0300

    Added Cart context in Navbar

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,11 +3,15 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
+
 
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty, onAdd } = useStateContext();
+
 
   return (
     <div>
@@ -44,16 +48,16 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick=""><AiOutlineMinus />
+              <span className="minus" onClick={decQty}><AiOutlineMinus />
               </span>
-              <span className="num" onClick="">0
+              <span className="num" onClick="">{qty}
               </span>
-              <span className="plus" onClick=""><AiOutlinePlus />
+              <span className="plus" onClick={incQty}><AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">Add to Cart</button>
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
             <button type="button" className="buy-now" onClick="">Buy Now</button>
           </div>
         </div>
